2bfdf7f601dd4f04dc3523aeb8cbe8e111017fd4
alancedano2 <supernatura94@yahoo.com>
2025-05-01 17:53:14 -0400

    Update epg.js

--- api/epg.js
+++ api/epg.js
@@ -1,4 +1,9 @@
-export default function handler(req, res) {
+import { gzip } from 'zlib';
+import { promisify } from 'util';
+
+const gzipAsync = promisify(gzip);
+
+export default async function handler(req, res) {
   const xml = `<?xml version="1.0" encoding="UTF-8" ?>
 <tv generator-info-name="kq105.vercel.app">
   <channel id="kq105tv">
@@ -6,27 +11,34 @@ export default function handler(req, res) {
     <icon src="https://bloximages.chicago2.vip.townnews.com/kq105.com/content/tncms/custom/image/23c304fa-a5bb-11ee-aed9-ab40ab64f7be.png" />
   </channel>
 
-  <programme start="20250501040000 -0400" stop="20250501010000 -0400" channel="kq105tv">
+  <programme start="20250501040000 -0400" stop="20250501060000 -0400" channel="kq105tv">
     <title lang="es">Videos Musicales Powered by KQ105</title>
   </programme>
-  <programme start="20250501010000 -0400" stop="20250501060000 -0400" channel="kq105tv">
+  <programme start="20250501060000 -0400" stop="20250501100000 -0400" channel="kq105tv">
     <title lang="es">KQ Al Aire con Héctor Ortiz</title>
   </programme>
-  <programme start="20250501060000 -0400" stop="20250501110000 -0400" channel="kq105tv">
+  <programme start="20250501100000 -0400" stop="20250501150000 -0400" channel="kq105tv">
     <title lang="es">KQOnline con Alex Diaz</title>
   </programme>
-  <programme start="20250501110000 -0400" stop="20250501140000 -0400" channel="kq105tv">
+  <programme start="20250501150000 -0400" stop="20250501180000 -0400" channel="kq105tv">
     <title lang="es">La Tendencia de Molusco con Ali, Pamela y Robert</title>
   </programme>
-  <programme start="20250501140000 -0400" stop="20250501150000 -0400" channel="kq105tv">
+  <programme start="20250501180000 -0400" stop="20250501190000 -0400" channel="kq105tv">
     <title lang="es">KQ Al Aire con Pedro Villegas</title>
   </programme>
-  <programme start="20250501150000 -0400" stop="20250502040000 -0400" channel="kq105tv">
+  <programme start="20250501190000 -0400" stop="20250502040000 -0400" channel="kq105tv">
     <title lang="es">Videos Musicales Powered by KQ105</title>
   </programme>
 </tv>`;
 
-  res.setHeader("Content-Type", "application/xml; charset=utf-8");
-  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-  res.status(200).send(xml);
+  try {
+    const compressed = await gzipAsync(xml);
+
+    res.setHeader('Content-Encoding', 'gzip');
+    res.setHeader('Content-Type', 'application/xml');
+    res.setHeader('Content-Disposition', 'inline; filename="epg.xml.gz"');
+    res.status(200).send(compressed);
+  } catch (error) {
+    res.status(500).send('Failed to compress EPG');
+  }
 }
